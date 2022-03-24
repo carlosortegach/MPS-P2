32236f7601b0505246bb620d64068592301c68bb
carlosortegach <cortegaestudios@uma.es>
2022-03-24 11:52:21 +0100

    Implementadas operaciones complejas menos sort

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 /**
  * Class representing a node of a double-ended queue (deque). Each node has pointers to the next and
  * previous nodes. The previous and next of the first and last node of the deque is null.
@@ -68,5 +70,61 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T>{
         }
         if(nodo1 == nodo2) l++;
         return l;
+    }
+
+    @Override
+    public DequeNode<T> getAt(int position) {
+        DequeNode<T> nodo1 = first;
+        if(nodo1 == null)
+            throw new RuntimeException("La lista es nula")
+        else if(position < 1 && position > this.size())
+            throw new RuntimeException("Posicion invalida")
+        for (int i = 1; i < position; i++) {
+            nodo1.getNext();
+        }
+        return nodo1;
+    }
+
+    @Override
+    public DequeNode<T> find(T item) {
+        DequeNode<T> nodo1 = first;
+        if(nodo1 == null)
+            throw new RuntimeException("La lista es nula")
+        while(nodo1 != null && !nodo1.getItem().equals(item)){
+            nodo1 = nodo1.getNext();
+        }
+        if(nodo1 == null)
+            throw new RuntimeException("Item no encontrado")
+        return nodo1;
+    }
+
+    @Override
+    public void delete(DequeNode<T> node) {
+        DequeNode<T> nodo1 = first;
+        while(nodo1 != null && !nodo1.equals(node)){
+            nodo1 = nodo1.getNext();
+        }
+        if(nodo1 == null)
+            throw new RuntimeException("Item no encontrado")
+        if(!nodo1.isNotATerminalNode()){
+            first = null;
+            last = null;
+        }
+        else if(nodo1.isLastNode()){
+            nodo1.getPrevious().setNext(null);
+            last = last.getPrevious();
+        }
+        else if(nodo1.isFirstNode())
+            nodo1.getNext().setPrevious(null);
+            first = first.getNext();
+        else {
+            nodo1.getPrevious().setNext(nodo1.getNext());
+            nodo1.getNext().setPrevious(nodo1.getPrevious());
+        }
+    }
+
+    @Override
+    public void sort(Comparator<?> comparator) {
+
     }
 }
